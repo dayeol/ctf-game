5f0eb7ce4a9e76f8345ad71c5566fff12f5bd4e1
Dayeol <loving2days@gmail.com>
2013-01-09 14:23:56 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CTF::Application.config.secret_token = '2575cea891f1aebf60c439a34b0dc08e4a95873e49d99c6ef59fc2f35d779581f0e56dd90750dc7ca72d35ce9aa2ee2b571f679d15f4dd8f5a9c06057746517c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CTF::Application.config.session_store :cookie_store, :key => '_CTF_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CTF::Application.config.session_store :active_record_store
